8fdf46ee5f60586f6f0c8cf3c8bb391dee79bf5f
Sergio <d.chile58@live.fr>
2025-02-11 11:38:02 +0100

    fix: corrections des tables liées aux tournois

--- Back/data/db-init.js
+++ Back/data/db-init.js
@@ -21,26 +21,56 @@ CREATE TABLE IF NOT EXISTS parties (
   partyName   varchar(64) NOT NULL UNIQUE
 )`;
 
+const createDungeonsTableQuery = `
+CREATE TABLE IF NOT EXISTS dungeons (
+  id          SERIAL PRIMARY KEY,
+  name        varchar(64) NOT NULL UNIQUE,
+  difficulty  integer NOT NULL,
+  timer       time NOT NULL
+)
+`;
+
 const createClassAndRolesTables = pool
   .query(createClassTableQuery)
+  .catch((err) => {
+    console.error("Erreur lors de la création de la table class :", err);
+    throw err;
+  })
+
   .then(() => {
     console.log("Table Class créée avec succès");
     return pool.query(createRolesTableQuery);
   })
+  .catch((err) => {
+    console.error("Erreur lors de la création de la table roles:", err);
+    throw err;
+  })
+  
   .then(() => {
     console.log("Table Roles créée avec succès");
     return pool.query(createPartiesTableQuery);
   })
+  .catch((err) => {
+    console.error("Erreur lors de la création de la table parties :", err);
+    throw err;
+  })
+
   .then(() => {
     console.log("Table Parties créée avec succès");
+    return pool.query(createDungeonsTableQuery);
   })
   .catch((err) => {
-    console.error("Erreur lors de la création des tables class, roles ou parties:", err);
+    console.error("Erreur lors de la création de la table dungeons:", err);
     throw err;
+  })
+
+  .then(() => {
+    console.log("Table Dungeons créée avec succès");
   });
 
   // Attendre que les tables roles et class soient créées avant de créer la table can_be
-  createClassAndRolesTables.then(() => {
+  createClassAndRolesTables
+  .then(() => {
     pool
       .query(
         `CREATE TABLE IF NOT EXISTS classRoles (
@@ -84,3 +114,40 @@ const createClassAndRolesTables = pool
           })
       })
   })
+  // attendre que la table dungeons soit créée avant de créer la table tournaments
+  .then(() => {
+    pool.query(
+        `CREATE TABLE IF NOT EXISTS tournaments (
+          id          SERIAL PRIMARY KEY,
+          startDate   date NOT NULL,
+          endDate     date NOT NULL,
+          dungeonId   integer NOT NULL,
+          CONSTRAINT fk_dungeon FOREIGN KEY(dungeonId) REFERENCES dungeons(id)
+        )
+        `
+    )
+    .catch((err) => {
+      console.error("Erreur lors de la création de la table tournaments:", err);
+    })
+    // attendre que les tables parties et tournaments soient créées avant de créer la table partiesTournaments
+    .then(() => {
+      console.log("Table tournaments créée avec succès");
+      pool.query(
+        `CREATE TABLE IF NOT EXISTS partiesTournaments (
+        partyId         integer NOT NULL,
+        tournamentId    integer NOT NULL,
+        timer           time,
+        CONSTRAINT fk_party FOREIGN KEY(partyId) REFERENCES parties(id),
+        CONSTRAINT fk_tournamenets FOREIGN KEY(tournamentId) REFERENCES tournaments(id),
+        CONSTRAINT partiestournaments_pkey PRIMARY KEY (partyId, tournamentId)
+      )`
+      )
+      .then(() => {
+        console.log("Table partiesTournaments créée avec succès");
+      })
+      .catch((err) => {
+        console.error("Erreur lors de la création de la table partiesTournaments:", err);
+      })
+    })
+  })
+  
